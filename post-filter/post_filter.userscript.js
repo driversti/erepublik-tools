ab3df9513fa03a4b48033609cf622f5d6ac21629
Yurii Chekhotskyi <driversti@gmail.com>
2023-07-10 18:32:08 +0200

    Revert "User locator"

--- post-filter/post_filter.userscript.js
+++ post-filter/post_filter.userscript.js
@@ -17,7 +17,7 @@
   'use strict';
 
   const bannedWords = [
-    "wordle","Congratulations, you entered the Top 100","*♥♥*","Link za glasanje"
+    "wordle","Congratulations, you entered the Top 100","*♥♥*"
   ]; // Add the words you want to filter out.
 
   function filterPosts() {
