5911df9c8f332c6224b3662168f3decc0820d0dd
Yurii Chekhotskyi <github@seniorjava.dev>
2023-09-04 17:35:31 +0200

    Country events add multiple (#9)
    * added 'config.json' for storing supported countries
    
    * now we can handle as many countries as we want

--- country-events/app.js
+++ country-events/app.js
@@ -3,78 +3,15 @@ let parser = new Parser();
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require("fs");
 const moment = require('moment-timezone');
+const config = require('./config');
+const hrtime = process.hrtime;
 
-const COUNTRY_ID = parseInt(process.env.COUNTRY_ID);
 const TELEGRAM_TOKEN = process.env.COUNTRY_TELEGRAM_TOKEN;
-const TELEGRAM_LAWS_CHANNEL = process.env.COUNTRY_LAWS_TELEGRAM_CHANNEL;
-const TELEGRAM_BATTLES_CHANNEL = process.env.COUNTRY_BATTLES_TELEGRAM_CHANNEL;
-
+const RSS_FEED_ALL = "https://www.erepublik.com/en/main/news/military/all/all/1/rss"
 const CHECK_INTERVAL = 10 * 1000; // Checking every 10 seconds
-const LATEST_LAW_FILE = 'latest_law_date.txt';
-const LATEST_BATTLE_FILE = 'latest_battle_date.txt';
 const bot = new TelegramBot(TELEGRAM_TOKEN);
 
-let latestPubDate = new Date(0); // Initialize with the earliest possible date.
-
-const COUNTRIES = {
-  map: new Map([
-    [19, {
-      name: 'Indonesia',
-      nativeName: 'Indonesia',
-      encodedName: 'Indonesia',
-      lang: 'en',
-      tz: 'Asia/Jakarta'
-    }],
-    [40, {
-      name: 'Ukraine',
-      nativeName: 'Україна',
-      encodedName: 'Ukraine',
-      lang: 'en',
-      tz: 'Europe/Kyiv'
-    }],
-    [57, {
-      name: 'Pakistan',
-      nativeName: 'Pakistan',
-      encodedName: 'Pakistan',
-      lang: 'en',
-      tz: 'Asia/Karachi'
-    }],
-    [72, {
-      name: 'Lithuania',
-      nativeName: 'Lithuania',
-      encodedName: 'Lithuania',
-      lang: 'en',
-      tz: 'Europe/Vilnius'
-    }]
-  ]),
-
-  getEncodedName: function (countryId) {
-    return this.map.get(countryId).encodedName;
-  },
-
-  getName: function (countryId) {
-    return this.map.get(countryId).name;
-  },
-
-  getNativeName: function (countryId) {
-    return this.map.get(countryId).nativeName;
-  },
-
-  getLang: function (countryId) {
-    return this.map.get(countryId).lang;
-  },
-
-  getTZ: function (countryId) {
-    return this.map.get(countryId).tz;
-  },
-
-  getRssFeed: function (countryId, page = 1) {
-    return `https://www.erepublik.com/${this.getLang(countryId)}/main/news/military/all/all/${page}/rss`;
-  }
-}
-
 const BROKEN_LINK_REGEX = new RegExp(`https:\/\/www\.erepublik\.com\<b\>.*\<\/b\>$`);
-const LAW_REGEX = new RegExp(`https://www.erepublik.com/(?:${COUNTRIES.getLang(COUNTRY_ID)}|en)/main/law/`);
 
 function checkForUpdates(url) {
   parser.parseURL(url)
@@ -83,83 +20,104 @@ function checkForUpdates(url) {
 }
 
 function readFeed(feed) {
+  const start = hrtime();
   let reversedItems = [...feed.items].reverse();
 
-  handleLaws(reversedItems);
-  handleBattles(reversedItems);
+  config.map.forEach((country) => {
+    handleLaws(reversedItems, country);
+    handleBattles(reversedItems, country);
+  });
+  const end = hrtime(start);
+  // console.info("Execution time (hr): %ds %dms", end[0], end[1] / 1000000);
 }
 
-function handleLaws(reversedItems) {
+function handleLaws(reversedItems, country) {
+  // console.log(`Handling ${JSON.stringify(country.name)} (${country.id}) laws`);
   const items = reversedItems
-    .filter(item => lawsOnly(item))
-    .filter(item => forCountryOnly(item))
-    .map(item => createClearItems(item))
+    .filter(item => lawsOnly(item, country.id))
+    .filter(item => forCountryOnly(item, country.id))
+    .map(item => prepareItemsForSending(item, country.id))
 
-  formatAndSend(items, TELEGRAM_LAWS_CHANNEL, LATEST_LAW_FILE);
-}
-
-function handleBattles(reversedItems) {
-  const items = reversedItems
-    .filter(item => battlesOnly(item))
-    .filter(item => forCountryOnly(item))
-    .map(item => createClearItems(item))
-
-  formatAndSend(items, TELEGRAM_BATTLES_CHANNEL, LATEST_BATTLE_FILE);
-}
-
-function formatAndSend(items, channel, fileName) {
   let messages = [];
 
   for (let item of items) {
     let itemDate = new Date(item.time);
-    if (itemDate > latestPubDate) {
+    if (itemDate > country.getLawsLastUpdated()) {
       let message = `${item.time}\n<a href="${item.url}">${item.text}</a>`;
       messages.push(message);
-      latestPubDate = itemDate;
-      saveLatestDate(fileName, latestPubDate);
+      country.setLawsLastUpdated(itemDate);
     }
   }
 
-  if (messages.length === 0) {
+  let message = messages.join("\n\n");
+  if (message.length <= 0) {
     return;
   }
+  send(message, country.battlesChannel);
+}
 
-  let combinedMessage = messages.join("\n\n");
-  bot.sendMessage(channel, combinedMessage, {
+function handleBattles(reversedItems, country) {
+  // console.log(`Handling ${JSON.stringify(country.name)} (${country.id}) battles`);
+  const items = reversedItems
+    .filter(item => battlesOnly(item, country.id))
+    .filter(item => forCountryOnly(item, country.id))
+    .map(item => prepareItemsForSending(item, country.id))
+
+  let messages = [];
+
+  for (let item of items) {
+    let itemDate = new Date(item.time);
+    if (itemDate > country.getBattlesLastUpdated()) {
+      let message = `${item.time}\n<a href="${item.url}">${item.text}</a>`;
+      messages.push(message);
+      country.setBattlesLastUpdated(itemDate);
+    }
+  }
+
+  let message = messages.join("\n\n");
+  if (message.length <= 0) {
+    return;
+  }
+  send(message, country.battlesChannel);
+}
+
+function send(message, channel) {
+  bot.sendMessage(channel, message, {
     parse_mode: 'HTML',
     disable_web_page_preview: true
   })
-    .then(() => console.log(combinedMessage))
+    .then(() => console.log(message))
     .catch(error => {
-      console.log(`${channel}, ${combinedMessage}`);
+      console.log(`${channel}, ${message}`);
       console.error("Error sending the message:", error);
     });
 }
 
-function battlesOnly(item) {
-  return hasResistanceStarted(item) || hasSecured(item) || hasAttacked(item);
+function battlesOnly(item, countryId) {
+  return hasResistanceStarted(item, countryId) || hasSecured(item, countryId) || hasAttacked(item, countryId);
 }
 
-function hasResistanceStarted(item) {
-  const regex = new RegExp(`https://www.erepublik.com/(?:${COUNTRIES.getLang(COUNTRY_ID)}|en)/wars/show/`);
+function hasResistanceStarted(item, countryId) {
+  const regex = new RegExp(`https://www.erepublik.com/(?:${config.getLang(countryId)}|en)/wars/show/`);
   return regex.test(item.link);
 }
 
-function hasSecured(item) {
-  const regex = new RegExp(`https://www.erepublik.com/(?:${COUNTRIES.getLang(COUNTRY_ID)}|en)/wars/show/`);
+function hasSecured(item, countryId) {
+  const regex = new RegExp(`https://www.erepublik.com/(?:${config.getLang(countryId)}|en)/wars/show/`);
   return regex.test(item.link);
 }
 
-function hasAttacked(item) {
-  const regex = new RegExp(`https://www.erepublik.com/(?:${COUNTRIES.getLang(COUNTRY_ID)}|en)/military/battlefield/`);
+function hasAttacked(item, countryId) {
+  const regex = new RegExp(`https://www.erepublik.com/(?:${config.getLang(countryId)}|en)/military/battlefield/`);
   return regex.test(item.link);
 }
 
-function lawsOnly(item) {
-  return lawLink(item) || brokenLink(item);
+function lawsOnly(item, countryId) {
+  return lawLink(item, countryId) || brokenLink(item, countryId);
 }
 
-function lawLink(item) {
+function lawLink(item, countryId) {
+  const LAW_REGEX = new RegExp(`https://www.erepublik.com/(?:${config.getLang(countryId)}|en)/main/law/`);
   return LAW_REGEX.test(item.link);
 }
 
@@ -167,14 +125,14 @@ function brokenLink(item) {
   return BROKEN_LINK_REGEX.test(item.link);
 }
 
-function forCountryOnly(item) {
-  return item.link.indexOf(COUNTRIES.getEncodedName(COUNTRY_ID)) !== -1
-    || item.content.indexOf(COUNTRIES.getName(COUNTRY_ID)) !== -1
-    || item.content.indexOf(COUNTRIES.getNativeName(COUNTRY_ID)) !== -1;
+function forCountryOnly(item, countryId) {
+  return item.link.indexOf(config.getEncodedName(countryId)) !== -1
+    || item.content.indexOf(config.getName(countryId)) !== -1
+    || item.content.indexOf(config.getNativeName(countryId)) !== -1;
 }
 
-function createClearItems(item) {
-  const tz = COUNTRIES.getTZ(COUNTRY_ID);
+function prepareItemsForSending(item, countryId) {
+  const tz = config.getTZ(countryId);
   const itemDate = moment(item.pubDate).tz(tz).format("YYYY-MM-DD HH:mm:ss");
   if (BROKEN_LINK_REGEX.test(item.link)) {
     const urlMatch = item.content.match(/<a href="([^"]+)">/);
@@ -196,21 +154,9 @@ function createClearItems(item) {
   return {text: item.content, url: item.link, time: itemDate};
 }
 
-function loadLatestDate(fileName) {
-  if (fs.existsSync(fileName)) {
-    const dateFromFile = fs.readFileSync(fileName, 'utf-8');
-    latestPubDate = dateFromFile && new Date(dateFromFile);
-  }
-}
 
-function saveLatestDate(fileName, date) {
-  fs.writeFileSync(fileName, date.toISOString());
-}
-
-loadLatestDate(LATEST_LAW_FILE);
-loadLatestDate(LATEST_BATTLE_FILE);
-checkForUpdates(COUNTRIES.getRssFeed(COUNTRY_ID));
-setInterval(() => checkForUpdates(COUNTRIES.getRssFeed(COUNTRY_ID)), CHECK_INTERVAL);
+checkForUpdates(RSS_FEED_ALL);
+setInterval(() => checkForUpdates(RSS_FEED_ALL), CHECK_INTERVAL);
 
 // checkForUpdates(COUNTRIES.getRssFeed(COUNTRY_ID, 2));
 // checkForUpdates(COUNTRIES.getRssFeed(COUNTRY_ID, 3));
